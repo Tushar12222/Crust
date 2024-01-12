9f959d5fc77aa4bb7e9ec9e087f661ec3c935bb0
Tushar GH <108387741+Tushar12222@users.noreply.github.com>
2024-01-12 12:57:05 +0530

    Updated main.js
    Added "_bg" so that it links to the wasm file.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import init, { run_app } from './pkg/{{crate_name}}.js';
 async function main() {
-   await init('./pkg/{{crate_name}}.wasm');
+   await init('./pkg/{{crate_name}}_bg.wasm');
    run_app();
 }
 main()
